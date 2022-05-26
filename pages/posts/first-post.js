a546a6bc4b91c4816daec1ca4aac228602371b11
Carlos Ernesto <carlosblinf@gmail.com>
2022-05-26 12:14:27 -0400

    change imports with js extension to the end

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
-import Layout from '../../components/layout';
+import Layout from '../../components/layout.js';
 
 export default function FirstPost() {
   return (
